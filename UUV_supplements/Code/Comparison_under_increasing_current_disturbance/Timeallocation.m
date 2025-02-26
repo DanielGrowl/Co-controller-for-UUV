06e4e3c11c7bd6b8288248e42705dcb51073ab2b
DanielGrowl <62696988+DanielGrowl@users.noreply.github.com>
2025-02-26 15:51:42 +0800

    Delete UUV_supplements directory

--- UUV_supplements/Code/Comparison_under_increasing_current_disturbance/Timeallocation.m
+++
@@ -1,5 +0,0 @@
-function Tasktime = Timeallocation(Initial_x, Desired_x, unit_time)
-    Euldistance = norm(Desired_x-Initial_x);
-    Euldistance = ceil(Euldistance);
-    Tasktime = unit_time * Euldistance;
-end
